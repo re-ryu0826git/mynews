0ddd70d3c8ddee25c9231bbf28d103a3f96746b7
”TanakaRyunosuke” <ryu0826tanaka@gmail.com>
2019-07-26 13:07:53 +0000

    kadai_11-3~8 profile create view

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,19 +1,17 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>プロフィール</title>
-    </head>
-    <body>
-        <section>
-            <h2>プロフィール</h2>
-            <p>田中 龍之介</p>
-            <p>PHPのLaravelでウェブサイトを構築中です。RailsよりRoutingの記述が難しかった。<br>
+@extends('layouts.profile')
+
+@section('title','プロィール')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール</h2>
+                <p>田中 龍之介</p>
+                <p>PHPのLaravelでウェブサイトを構築中です。RailsよりRoutingの記述が難しかった。<br>
             フレームワークは覚えることが多いので、1つ1つ理解して学習を進めていく。<br>
             今後はオリジナルアプリを作る予定です。</p>
-            <button type="submit">変更</button>
-        </section>
-    </body>
-</html>
+            </div>
+        </div>
+    </div>
+@endsection
