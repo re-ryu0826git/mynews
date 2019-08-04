e467b7699228e94a6ced6bdabe47bf8930e7cab8
”TanakaRyunosuke” <ryu0826tanaka@gmail.com>
2019-08-04 05:44:32 +0000

    delete view create

--- routes/web.php
+++ routes/web.php
@@ -47,8 +47,8 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
      Route::get('news', 'Admin\NewsController@index');
      Route::get('news/edit', 'Admin\NewsController@edit');
      Route::post('news/edit', 'Admin\NewsController@update');
+     Route::get('news/delete', 'Admin\NewsController@delete');
      Route::get('profile/create', 'Admin\ProfileController@add');
      Route::post('profile/create', 'Admin\ProfileController@create');
      Route::post('profile/edit', 'Admin\ProfileController@update');
-
 });
