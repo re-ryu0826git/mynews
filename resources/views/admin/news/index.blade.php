b3ba7792d62f5f3d96fe99b7809ede2a71974a8f
”TanakaRyunosuke” <ryu0826tanaka@gmail.com>
2019-08-04 04:53:25 +0000

    edit update view create

--- resources/views/admin/news/index.blade.php
+++ resources/views/admin/news/index.blade.php
@@ -37,6 +37,7 @@
                                 <th width="10%">ID</th>
                                 <th width="20%">タイトル</th>
                                 <th width="50%"></th>
+                                <th width="10%"></th>
                             </tr>
                         </thead>
                         <tbody>
@@ -45,6 +46,11 @@
                                     <th>{{ $news->id }}</th>
                                     <td>{{ str_limit($news->title, 100) }}</td>
                                     <td>{{ str_limit($news->body, 250) }}</td>
+                                    <td>
+                                        <div>
+                                            <a href="{{ action('Admin\NewsController@edit', ['id' => $news->id]) }}">編集</a>
+                                        </div>
+                                    </td>
                                 </tr> 
                             @endforeach
                         </tbody>
