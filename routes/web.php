87f1850b4aad6a86a0e927d1e965942041b5c7e1
”TanakaRyunosuke” <ryu0826tanaka@gmail.com>
2019-08-24 03:13:50 +0000

    General user news view

--- routes/web.php
+++ routes/web.php
@@ -55,3 +55,5 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
      Route::get('profile/edit', 'Admin\ProfileController@edit');
      Route::post('profile/edit', 'Admin\ProfileController@update');
 });
+
+Route::get('/', 'NewsController@index');
